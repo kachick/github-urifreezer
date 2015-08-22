747979c0093a235de6000abe6bff5ed859dc2108
Kenichi Kamiya <kachick1@gmail.com>
2015-08-23 05:41:56 +0900

    Use thor for usages

--- bin/ghuf.rb
+++ bin/ghuf.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
 # coding: us-ascii
 
+require 'thor'
 require_relative '../lib/github/urifreezer'
 
-puts Github::URIFreezer.fix(ARGV.shift)
+class GhUF < Thor
+  desc 'based-URI', 'Print the fixed URI under latest commit of the branch'
+  def fix(uri)
+    puts Github::URIFreezer.fix(uri)
+  end
+end
+
+GhUF.start ['fix', *ARGV]

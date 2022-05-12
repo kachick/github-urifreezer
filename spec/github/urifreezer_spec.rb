91ddf3ba43cd04844e1bcc8c4795b0acba7cc6bc
Kenichi Kamiya <kachick1@gmail.com>
2022-05-12 14:16:57 +0900

    Make stable specs

--- spec/github/urifreezer_spec.rb
+++ spec/github/urifreezer_spec.rb
@@ -4,8 +4,8 @@
 require_relative '../spec_helper'
 
 RSpec.describe GitHub::URIFreezer do
-  let(:target) { 'https://github.com/kachick/striuct/blob/gh-pages/yard/file.MIT-LICENSE.html#L6-L11' }
-  let(:result) { URI.parse 'https://github.com/kachick/striuct/blob/0e089fe7f97c444a4c5ba3a571d300078b87d17e/yard/file.MIT-LICENSE.html#L6-L11' }
+  let(:target) { 'https://github.com/kachick/github-urifreezer/blob/github-urlfreezer-0.0.1/Rakefile#L6' }
+  let(:result) { URI.parse 'https://github.com/kachick/github-urifreezer/blob/6c55bcd6721b1bdff99cbb9a5241b5f38f7eb062/Rakefile#L6' }
 
   describe '.fix' do
     it { expect(GitHub::URIFreezer.fix(target)).to eq(result) }
